fc77bba2a8b6b76c6e92138770538398d7f8ba3a
Brian Kim <briankimpossible@gmail.com>
2014-11-13 01:53:58 -0500

    Second poop

--- src/main.js
+++ src/main.js
@@ -1,16 +1,38 @@
 /** @jsx React.DOM */
 var React = require('react');
 
-var App = require('./App');
-var Home = require('./Home');
 var About = require('./About');
-var Lipogram = require('./Lipogram');
+var Home = require('./Home');
+var {Anagram, Lipogram} = require('./Gram');
 var {DefaultRoute, Route, Routes} = require('react-router');
+var React = require('react');
+var {Link} = require('react-router');
+require('./App.css');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <header>
+          <ul>
+            <li><Link to="home">Home</Link></li>
+            <li><Link to="about">About</Link></li>
+            <li><Link to="lipogram">Lipograms</Link></li>
+            <li><Link to="anagram">Anagrams</Link></li>
+          </ul>
+        </header>
+
+        <this.props.activeRouteHandler />
+      </div>
+    );
+  }
+});
 
 React.renderComponent((
   <Routes location="hash">
     <Route path="/" handler={App}>
-      <DefaultRoute name="home" handler={Home} />
+      <Route name="home" handler={Home} />
+      <Route name="anagram" handler={Anagram} />
       <Route name="lipogram" handler={Lipogram} />
       <Route name="about" handler={About} />
     </Route>
